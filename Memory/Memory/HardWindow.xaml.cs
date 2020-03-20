28b729e3df5468556ed57e39ab751848405a092c
SkyR1seR <56387043+SkyR1seR@users.noreply.github.com>
2020-03-20 23:04:02 +0400

    Memory UI

---
+++ Memory/Memory/HardWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Memory
+{
+    /// <summary>
+    /// Логика взаимодействия для EasyWindow.xaml
+    /// </summary>
+    public partial class HardWindow : Window
+    {
+        public HardWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow MainWindow = new MainWindow();
+            MainWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Memory/Memory/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Memory
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EasyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            EasyWindow EasyWindow = new EasyWindow();
+            EasyWindow.Show();
+            this.Close();
+        }
+
+        private void HardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            HardWindow HardWindow = new HardWindow();
+            HardWindow.Show();
+            this.Close();
+        }
+    }
+}
